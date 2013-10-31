ca41fe48646278d6dfad8e80daf405ddc3ad9eab
yosuke <taktakyou@gmail.com>
2013-11-01 08:47:29 +0900

    ちょっと変更

--- src/pkagent/ExpRobo.java
+++ src/pkagent/ExpRobo.java
@@ -41,7 +41,7 @@ public class ExpRobo implements Agent, DefNum {
 		gp = new Point2D.Double(INIT_AX, INIT_AY);
 
 		try {
-			reader = new BufferedReader(new FileReader("exp/a2.csv"));
+			reader = new BufferedReader(new FileReader("exp/a9_0830.csv"));
 			
 			while(true) {
 				try{
